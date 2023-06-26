661821f1974a082a24f2b9f7c28021c0f1a9121e
Alexander Camargo <119891273+UltimaSL@users.noreply.github.com>
2023-06-26 11:29:14 -0500

    Add files via upload

---
+++ Semestral Programacion/Semestral R/Barco.java
@@ -0,0 +1,15 @@
+public class Barco {
+    private boolean seleccionado;
+    private int tamaño;
+
+    public Barco(boolean seleccionado, int tamaño) {
+        this.seleccionado = seleccionado;
+        this.tamaño = tamaño;
+    }
+    getTamaño(){
+
+    }
+    public boolean setSeleccionado(){
+
+    }
+}
