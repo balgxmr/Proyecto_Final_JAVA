4bda9e6c060b6d1ba03bb21d7ad1c5b7aa23c8a5
WwIiNnEeRrDd <124347286+WwIiNnEeRrDd@users.noreply.github.com>
2023-07-03 22:35:46 -0500

    Add files via upload

---
+++ Semestral Programacion/Jugador.java
@@ -0,0 +1,26 @@
+public class Jugador {
+    private String name;
+    private Tablero tablero;
+    private Barco[] barcos = new Barco[4];
+
+    public Jugador(String n) {
+        this.name = n;
+        this.tablero = new Tablero();
+        this.barcos[0] = new Barco(4);
+        this.barcos[1] = new Barco(3);
+        this.barcos[2] = new Barco(2);
+        this.barcos[3] = new Barco(2);
+    }
+
+    public Tablero getTablero() {
+        return tablero;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Barco getBarco(int index) {
+        return barcos[index];
+    }
+}
